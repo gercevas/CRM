8c045f16650fc8cc14f324a168660b19dac3ef5c
Germán Cevallos <gcevallosmeneses@gmail.com>
2025-06-30 21:02:03 +0200

    Schema

--- frontend/schemas/invoice_schema.py
+++ frontend/schemas/invoice_schema.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
+
 
 class InvoiceCreate(BaseModel):
     user_email: str = Field(..., example="usuario@example.com")
@@ -9,13 +10,15 @@ class InvoiceCreate(BaseModel):
   
 class InvoiceResponse(BaseModel):
     id: str
-    user_email: str
+    user_email: EmailStr
     description: str
     amount: float
     status_code: str
     status: str
     created_at: str
 
+    class Config:
+        from_attribues = True
 class ResumenFinanciero(BaseModel):
     total_facturas: int
     total_monto: float
